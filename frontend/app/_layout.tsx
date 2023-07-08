b3ab5d564132ac8adc557c12d30ca27cf28541bb
yolisses <iamurise@gmail.com>
2023-07-07 21:16:37 -0300

    fix map display

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,12 +1,11 @@
 import { Slot } from "expo-router";
-import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AuthContextProvider } from "../auth/authContext";
 
 export default function Root() {
   return (
     <AuthContextProvider>
-      <SafeAreaView>
+      <SafeAreaView style={{ flex: 1 }}>
         <Slot />
       </SafeAreaView>
     </AuthContextProvider>
