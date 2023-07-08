54867d5960d85e8856d9571858c6344be5bae0d0
yolisses <iamurise@gmail.com>
2023-07-08 10:36:16 -0300

    add commentOptions modal

--- frontend/comment/commentOptions.tsx
+++ frontend/comment/commentOptions.tsx
@@ -1,10 +1,55 @@
 import { FontAwesome5 } from "@expo/vector-icons";
-import { TouchableHighlight } from "react-native";
+import { useState } from "react";
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function CommentOptions() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+  }
+
   return (
-    <TouchableHighlight style={{ marginLeft: "auto" }}>
-      <FontAwesome5 name="ellipsis-h" size={24} color="gray" />
-    </TouchableHighlight>
+    <>
+      <TouchableOpacity style={{ marginLeft: "auto" }} onPress={open}>
+        <FontAwesome5 name="ellipsis-h" size={24} color="gray" />
+      </TouchableOpacity>
+
+      <Modal
+        visible={isOpen}
+        animationType="slide"
+        onRequestClose={close}
+        presentationStyle="overFullScreen"
+      >
+        <View>
+          <TouchableOpacity style={styles.button}>
+            <FontAwesome5 name="trash" size={24} color="red" />
+            <Text style={styles.text}>Apagar comentário</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <FontAwesome5 name="pen" size={24} color="gray" />
+            <Text style={styles.text}>Editar comentário</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={close}>
+            <Text style={styles.text}>Cancelar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 16,
+  },
+  button: {
+    gap: 10,
+    padding: 10,
+    flexDirection: "row",
+  },
+});
