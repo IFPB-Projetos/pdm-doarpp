14a1c656754466b23825145e57eaa4bef36d5d9c
yolisses <iamurise@gmail.com>
2023-07-07 23:19:06 -0300

    replace mock auth user

--- mock/server.js
+++ mock/server.js
@@ -11,13 +11,13 @@ server.post("/echo", (req, res) => {
 
 server.post("/auth/login", (req, res) => {
   return res.jsonp({
-    id: "1",
-    name: "Gustavo Silva",
-    email: "gustavo.silva@example.com",
-    phone: "+55 21 91234-5678",
-    createdAt: "2023-07-06T14:30:00Z",
+    id: "11",
+    name: "Fernando Santos",
+    email: "fernando.santos@example.com",
+    phone: "+55 84 91234-5678",
+    createdAt: "2023-07-07T12:45:00Z",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem.",
+      "Fernando Santos is a passionate software engineer with over 5 years of experience in building web applications. He specializes in front-end development and has worked on numerous projects using technologies like React, Angular, and Vue.js. Fernando is a strong advocate for clean and maintainable code, and he always strives to deliver high-quality solutions that meet the clients' requirements. In his free time, he enjoys playing guitar, reading books, and exploring new technologies.",
   });
 });
 
