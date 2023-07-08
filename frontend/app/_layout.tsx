bbcd1be65ed7a8a6295e6f9e847fe237e9ad8994
yolisses <iamurise@gmail.com>
2023-07-07 21:00:27 -0300

    add safeAreaView

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Slot } from "expo-router";
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { AuthContextProvider } from "../auth/authContext";
 
 export default function Root() {
   return (
     <AuthContextProvider>
-      <Slot />
+      <SafeAreaView>
+        <Slot />
+      </SafeAreaView>
     </AuthContextProvider>
   );
 }
