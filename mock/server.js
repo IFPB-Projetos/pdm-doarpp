11478b2e9a6c7b22ab9cf771c85750b0ee8e455d
yolisses <iamurise@gmail.com>
2023-07-08 06:37:48 -0300

    fix mock auth route

--- mock/server.js
+++ mock/server.js
@@ -11,19 +11,21 @@ server.post("/echo", (req, res) => {
 
 server.post("/auth/login", (req, res) => {
   return res.jsonp({
-    id: "11",
-    name: "Fernando Santos",
-    email: "fernando.santos@example.com",
-    phone: "+55 84 91234-5678",
-    createdAt: "2023-07-07T12:45:00Z",
-    description:
-      "Fernando Santos is a passionate software engineer with over 5 years of experience in building web applications. He specializes in front-end development and has worked on numerous projects using technologies like React, Angular, and Vue.js. Fernando is a strong advocate for clean and maintainable code, and he always strives to deliver high-quality solutions that meet the clients' requirements. In his free time, he enjoys playing guitar, reading books, and exploring new technologies.",
+    user: {
+      id: "11",
+      name: "Fernando Santos",
+      email: "fernando.santos@example.com",
+      phone: "+55 84 91234-5678",
+      createdAt: "2023-07-07T12:45:00Z",
+      description:
+        "Fernando Santos is a passionate software engineer with over 5 years of experience in building web applications. He specializes in front-end development and has worked on numerous projects using technologies like React, Angular, and Vue.js. Fernando is a strong advocate for clean and maintainable code, and he always strives to deliver high-quality solutions that meet the clients' requirements. In his free time, he enjoys playing guitar, reading books, and exploring new technologies.",
+    },
+    token: "sfnkdankfdandiifornkwekfknri",
   });
 });
 
 server.use(
   jsonServer.rewriter({
-    "/auth/login": "/users/1",
     "/ngos/:id": "/ngos/:id?_embed=posts",
     "/posts/:id": "/posts/:id?_expand=ngo",
   })
