40f66bfef8744e29850074d24ac00690dd88d35e
Diuari <diuaripessoa07@gmail.com>
2023-07-08 18:16:21 -0300

    criando pagina de localizacao

--- frontend/user/editRouter.tsx
+++ frontend/user/editRouter.tsx
@@ -1,33 +1,75 @@
 import { LocationObject, getCurrentPositionAsync, requestForegroundPermissionsAsync } from 'expo-location';
-import { useState } from 'react';
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-async function requestPermision() {
-
-    const [location, setLocaion] = useState<LocationObject | null>(null);
+async function requestPermissionAndLocation() {
     const { granted } = await requestForegroundPermissionsAsync();
 
     if (granted) {
-
         const currentPosition = await getCurrentPositionAsync();
-
+        return currentPosition;
+    } else {
+        return null;
     }
-
 }
-
 
 export function PegarLocal() {
+    const [location, setLocation] = useState<LocationObject | null>(null);
+
+    useEffect(() => {
+        const getLocation = async () => {
+            const currentPosition = await requestPermissionAndLocation();
+            setLocation(currentPosition);
+        };
+        getLocation();
+    }, []);
+
+    if (!location) {
+        return <Text>Obtendo localização...</Text>;
+    }
 
     return (
-
         <View>
-
             <Text>Geo</Text>
+            <Text>Latitude: {location.coords.latitude}</Text>
+            <Text>Longitude: {location.coords.longitude}</Text>
+            {location &&
+                <MapView
+
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                        latitudeDelta: 0.005,
+                        longitudeDelta: 0.005,
+                    }}
+
+                >
+
+                    <Marker
+
+                        coordinate={{
+
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+
+                        }}
+                    />
+
+                </MapView>
+
+
+            }
 
         </View>
-
-
-    )
-
-
+    );
 }
+
+const styles = StyleSheet.create({
+    map: {
+        width: "80%",
+        height: "80%",
+    },
+});
+
