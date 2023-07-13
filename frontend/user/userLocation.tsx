36b5f698c719b47581d2e36efa70aecc94d81e69
Diuari <diuaripessoa07@gmail.com>
2023-07-13 08:43:06 -0300

    voltando ao normmal

--- frontend/user/userLocation.tsx
+++ frontend/user/userLocation.tsx
@@ -1,14 +1,38 @@
-import { useRouter } from "expo-router";
-import { useRef } from "react";
-import { Button, StyleSheet, View } from "react-native";
-import MapView from "react-native-maps";
+import {
+  LocationObject,
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync,
+} from "expo-location";
+import { useEffect, useRef, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 import { useAuth } from "../auth/authContext";
 import { api } from "../common/api";
 
+async function requestPermissionAndLocation() {
+  const { granted } = await requestForegroundPermissionsAsync();
+
+  if (granted) {
+    const currentPosition = await getCurrentPositionAsync();
+    return currentPosition;
+  } else {
+    return null;
+  }
+}
+
 export function UserEditLocalization() {
+  const user = useAuth().user;
+  const [location, setLocation] = useState<LocationObject | null>(null);
   const mapRef = useRef<MapView>(null);
-  const router = useRouter();
-  const { user } = useAuth();
+  const id = user?.id;
+
+  useEffect(() => {
+    const getLocation = async () => {
+      const currentPosition = await requestPermissionAndLocation();
+      setLocation(currentPosition);
+    };
+    getLocation();
+  }, []);
 
   async function handlerPress() {
     if (!mapRef.current) return;
@@ -17,18 +41,34 @@ export function UserEditLocalization() {
       center: { latitude, longitude },
     } = camera;
     await api.patch("/users/me/location", { latitude, longitude });
-    router.replace("/users/me");
+  }
+
+  if (!location) {
+    return <Text>Obtendo localização...</Text>;
   }
 
   return (
     <View style={styles.container}>
       {location && (
         <MapView
-          rotateEnabled={false}
           ref={mapRef}
           style={styles.map}
-        ></MapView>
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          />
+        </MapView>
       )}
+
       <Button title="atualizar cordenada" onPress={handlerPress}></Button>
     </View>
   );
